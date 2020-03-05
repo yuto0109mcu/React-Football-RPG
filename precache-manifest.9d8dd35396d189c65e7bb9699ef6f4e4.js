4e6bf904f44e1c67bae4d542ec2b8aba5ee78fbe
Yuto <yuto0109MCU@gmail.com>
2020-03-06 02:57:04 +0900

    Updates

---
+++ precache-manifest.9d8dd35396d189c65e7bb9699ef6f4e4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07949c5ced178edf434628f53290917",
+    "url": "/football-rpg/index.html"
+  },
+  {
+    "revision": "5cbe2496d02b2ed35f64",
+    "url": "/football-rpg/static/css/main.978c1050.chunk.css"
+  },
+  {
+    "revision": "c2d1f15d0dbfa06492cf",
+    "url": "/football-rpg/static/js/2.3752efcd.chunk.js"
+  },
+  {
+    "revision": "96737259fba8bed590bf9fffa61f905c",
+    "url": "/football-rpg/static/js/2.3752efcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cbe2496d02b2ed35f64",
+    "url": "/football-rpg/static/js/main.5a93202f.chunk.js"
+  },
+  {
+    "revision": "dcce2bcb47b05bc25dea",
+    "url": "/football-rpg/static/js/runtime-main.1657bdba.js"
+  },
+  {
+    "revision": "872a4d29992fe7aa29b42d32c84e8f68",
+    "url": "/football-rpg/static/media/battle-bg.872a4d29.jpg"
+  },
+  {
+    "revision": "575c86dd57731177a5d4e78e64b9b686",
+    "url": "/football-rpg/static/media/hmm.575c86dd.PNG"
+  },
+  {
+    "revision": "9ff51fc78cc4cdb99c98578ab36d407d",
+    "url": "/football-rpg/static/media/sad.9ff51fc7.PNG"
+  },
+  {
+    "revision": "d9884610d9448e7190279257b8797fc4",
+    "url": "/football-rpg/static/media/teki.d9884610.png"
+  },
+  {
+    "revision": "895d07a61fb0c856706aee47c51e5fd7",
+    "url": "/football-rpg/static/media/waoh.895d07a6.PNG"
+  },
+  {
+    "revision": "a839a231da08ea6a2eef325202a2bcd4",
+    "url": "/football-rpg/static/media/yeah.a839a231.PNG"
+  }
+]);
